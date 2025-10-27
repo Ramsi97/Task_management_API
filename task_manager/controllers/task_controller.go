51aa2b80f5c548ed620835e6304a5d656ab21fb0
Ramsi97 <ramadan2jemal@gmail.com>
2025-10-27 15:42:49 +0300

    services implemented

--- task_manager/controllers/task_controller.go
+++ task_manager/controllers/task_controller.go
@@ -3,94 +3,88 @@ package controllers
 import (
 	"net/http"
 	"strconv"
+	"task_manager/data"
 	"task_manager/models"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetTasks(c *gin.Context){
-	c.IndentedJSON(http.StatusOK, models.Tasks)
+func GetTasks(c *gin.Context) {
+	all := data.GetAllTask()
+	c.IndentedJSON(http.StatusOK, gin.H{"tasks": all})
 }
 
 func GetTask(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 
-	if(err != nil) {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error" : "Invalid task Id"})
-		return 
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid task Id"})
+		return
 	}
 
-	for _, task := range models.Tasks{
-		if(task.ID == id){
-			c.IndentedJSON(http.StatusOK, task)
-			return
-		}
+	task, err := data.GetTaskByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+
+	c.IndentedJSON(http.StatusOK, task)
 }
 
-func CreateTask(c *gin.Context){
-	var newTask := models.Task
+func CreateTask(c *gin.Context) {
+	var newTask models.Task
 
-	if err := c.ShouldBindJSON(&newTask); err != nil{
+	if err := c.ShouldBindJSON(&newTask); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid Json body"})
 		return
 	}
 
-	newTask.ID = len(models.Tasks) + 1
-	newTask.CreatedAt = time.Now()
-	newTask.UpdatedAt = time.Now()
-
-	models.Tasks := append(models.Tasks, newTask)
-	c.IndentedJSON(http.StatusCreated, newTask)
+	if newTask.Title == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Title is required"})
+		return
+	}
+	created := data.CreateTask(newTask)
+	c.IndentedJSON(http.StatusCreated, created)
 
 }
 
-func UpdateTask(c *gin.Context){
+func UpdateTask(c *gin.Context) {
 
-	id, err = strconv.Atoi(c.Param("id"))
+	id, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid Task Id"})
 		return
 	}
 
-	var updatedTask models.Task 
+	var updatedTask models.Task
 
 	if err := c.ShouldBindJSON(&updatedTask); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid Json body"})
 		return
 	}
 
-	for i, task := range models.Tasks {
-		if task.ID == id{
-			updatedTask.ID = id
-			updatedTask.CreatedAt = task.CreatedAt
-			updatedTask.UpdatedAt = time.Now()
-			models.Tasks[i] = updatedTask
-			c.IndentedJSON(http.StatusOK, updatedTask)
-			return
-		}
+	updated, err := data.UpdateTask(id, updatedTask)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
 	}
 
-	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+	c.IndentedJSON(http.StatusOK, updated)
+
 }
 
-func DeleteTask(c *gin.Context){
-	id, err = strconv.Atoi(c.Params("id"))
+func DeleteTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid Task ID"})
 		return
 	}
 
-	for i, task := range models.Tasks {
-		if task.ID == id {
-			models.Tasks = append(models.Tasks[:i], models.Tasks[i+1: ]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "Task deleted"})
-			return
-		}
+	if err := data.DeleteTask(id); err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
 	}
 
-	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "task removed"})
+
 }
--- task_manager/data/task_services.go
+++ task_manager/data/task_services.go
@@ -1,1 +1,92 @@
 package data
+
+import (
+	"errors"
+	"sync"
+	"task_manager/models"
+	"time"
+)
+
+var (
+	tasksMu sync.RWMutex
+	lastID  = 0
+)
+
+func GetAllTask() []models.Task {
+	tasksMu.RLock()
+	defer tasksMu.RUnlock()
+
+	copyTasks := make([]models.Task, len(models.Tasks))
+	copy(copyTasks, models.Tasks)
+	return copyTasks
+}
+
+func GetTaskByID(id int) (models.Task, error) {
+	tasksMu.RLock()
+	defer tasksMu.RUnlock()
+
+	for _, task := range models.Tasks {
+		if task.ID == id {
+			return task, nil
+		}
+	}
+
+	return models.Task{}, errors.New("task not found")
+}
+
+func CreateTask(input models.Task) models.Task {
+
+	tasksMu.Lock()
+	defer tasksMu.Unlock()
+
+	lastID++
+	input.ID = lastID
+	input.CreatedAt = time.Now()
+	input.UpdatedAt = input.CreatedAt
+
+	if input.DueDate.IsZero() {
+		input.DueDate = input.CreatedAt
+	}
+
+	models.Tasks = append(models.Tasks, input)
+	return input
+
+}
+
+func UpdateTask(id int, update models.Task) (models.Task, error) {
+
+	tasksMu.Lock()
+	defer tasksMu.Unlock()
+
+	for i, task := range models.Tasks {
+		if task.ID == id {
+
+			update.ID = id
+			update.CreatedAt = task.CreatedAt
+			update.UpdatedAt = time.Now()
+
+			if update.DueDate.IsZero() {
+				update.DueDate = task.DueDate
+			}
+			models.Tasks[i] = update
+			return models.Tasks[i], nil
+
+		}
+	}
+
+	return models.Task{}, errors.New("task not found")
+}
+
+func DeleteTask(id int) error {
+	tasksMu.Lock()
+	defer tasksMu.Unlock()
+
+	for i, task := range models.Tasks {
+		if id == task.ID {
+			models.Tasks = append(models.Tasks[:i], models.Tasks[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("task not found")
+}
